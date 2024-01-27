6ce594f9ef6a8747f897df48f12d2d6abe4c6dd5
Rahul Sen <rsen253@gmail.com>
2024-01-27 11:11:03 +0530

    Added navigation component added links to navigate different pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router";
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
 const Shop = () => <div>I am from shop page</div>;
@@ -6,8 +7,9 @@ const Shop = () => <div>I am from shop page</div>;
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/shop" element={<Shop />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
