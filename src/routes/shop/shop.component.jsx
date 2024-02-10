ee0038640636a30e1aace669d8ddb0a60bbff72f
Rahul Sen <rsen253@gmail.com>
2024-02-10 15:02:47 +0530

    created separate component for displaying 4 products for each category

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import { useContext, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 import "./shop.styles.scss";
 
 const Shop = () => {
@@ -8,18 +8,14 @@ const Shop = () => {
   // console.log("shop component");
   // console.log(categoriesMap["hats"]);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map((title) => (
-        <Fragment key={title}>
-          <h2>{title}</h2>
-          <div className="products-container">
-            {categoriesMap[title].map((product) => (
-              <ProductCard product={product} key={product.id} />
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map((title) => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 
